703289155f4004e2d909bf00a8c93e4da8647826
Yana Makogon <yana.mak89@gmail.com>
2026-03-16 12:19:03 +0100

    create sign in, sign up and forgot password pages and oauth component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom"
+import OAuth from "../component/OAuth";
+
+
 
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
   return (
-    <div>
-      Sign In
-    </div>
-  )
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+
+      <div className="flex justify-center flex-wrap items-center gap-8 px-5 py-11 max-w-6xl mx-auto">
+        
+        <div className="w-full md:w-[57%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1661754922567-1cc4891cd709?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="key"
+            className="w-full rounded-2xl"
+          />
+        </div>
+
+        <div className="w-full md:w-[57%] lg:w-[40%]">
+          <form>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+              className="mb-6 w-full px-4 py-2 text-xl  border-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+                className="mb-6 w-full px-4 py-2 text-xl border border-gray-300 rounded transition ease-in-out"
+              />
+
+              <span
+                className="absolute right-3 top-3 cursor-pointer text-gray-600"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">Don't have an account?
+                <Link to="/sign-up" className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1">Register</Link >
+              </p>
+              <p><Link to="/forgot-password" className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out">Forgot password?</Link></p>
+              </div>
+              <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800" type="submit">
+                Sign in
+              </button>
+              <div className="flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+          
+          
+        </div>
+
+      </div>
+    </section>
+  );
 }
